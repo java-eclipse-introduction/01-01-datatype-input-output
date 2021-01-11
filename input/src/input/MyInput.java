b5a19f98e7112315b959b8212374a81cf109f783
Csaba Gyuris <gyuris.csaba@vasvari.org>
2021-01-11 16:34:51 +0100

    MyInput osztály main() függvénnyel.

---
+++ input/src/input/MyInput.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package input;
+
+/**
+ * @author Gyuris Csaba
+ *
+ */
+public class MyInput {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Helló világ!");
+	}
+
+}
