3197815dfd683e0a1100abad6c48f2813a00e9b7
Csaba Gyuris <gyuris.csaba@vasvari.org>
2021-01-12 10:05:05 +0100

    MyOutput osztály main() fügvénnyel.

---
+++ output/src/output/MyOutput.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package output;
+
+/**
+ * @author Gyuris Csaba
+ *
+ */
+public class MyOutput {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
