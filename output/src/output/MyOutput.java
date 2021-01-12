6fed48519bfcb3dd83c6f1ba6e6bf68e945c710f
Csaba Gyuris <gyuris.csaba@vasvari.org>
2021-01-12 10:27:56 +0100

    O1.02 A megoldásban a System osztály használjuk.

--- output/src/output/MyOutput.java
+++ output/src/output/MyOutput.java
@@ -22,6 +22,11 @@ public class MyOutput {
 		int z=x+y;
 		
 		//O1.01 Adja meg azt az utasítást amely kiírja a c változó értékét a következõ formában: A havi bevétel 5000 forint.
+		
+		//O1.02 A megoldásban a System osztály használjuk.
+		//01.02 A kiírásnál a karakterláncot összefûzzük az egész típusú változóval.
+		//01.02 A sor végére újsor karaktert kerül.
+		System.out.println("A havi bérlet "+z+" forint.");		
 
 	}
 
