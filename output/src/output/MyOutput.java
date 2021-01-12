04b61316eb543504c3808bc3cabceff84905dd0f
Csaba Gyuris <gyuris.csaba@vasvari.org>
2021-01-12 10:24:35 +0100

    O1.01 Feladat

--- output/src/output/MyOutput.java
+++ output/src/output/MyOutput.java
@@ -14,6 +14,14 @@ public class MyOutput {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		//O1.01 Feladat
+		//O1.01 Adott a következõ programrészlet:
+		int x=3000;
+		int y=2000;
+		int z=x+y;
+		
+		//O1.01 Adja meg azt az utasítást amely kiírja a c változó értékét a következõ formában: A havi bevétel 5000 forint.
 
 	}
 
